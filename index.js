b78f11ee9f1e692ba96f472171ada5050dd92faa
Max Malm <benjick@dumfan.net>
2020-02-17 09:36:19 +0100

    Fix path bug

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ const {argv} = require('yargs')
   .demandOption(['c'])
   .alias('c', 'config')
   .describe('c', 'File to make the change in')
+  .coerce(['c'], path.resolve)
   .default('target', 'GIT_COMMIT')
   .alias('t', 'target')
   .describe('t', 'String to replace with commit id')
   .usage('Usage: $0 [options]')
   .epilog('Kodbruket 2020')
 
-const filepath = path.resolve(__dirname, argv.config)
-let file = fs.readFileSync(filepath, 'utf8')
+let file = fs.readFileSync(argv.config, 'utf8')
+console.log('argv.config', argv.config)
 let revision = process.execSync('git rev-parse HEAD').toString().trim()
 const dirty = process.execSync('git status -s').toString().length > 0
 if (dirty) {
   revision += '-dirty'
 }
 file = file.replace(argv.target, revision)
-fs.writeFileSync(filepath, file)
+fs.writeFileSync(argv.config, file)
